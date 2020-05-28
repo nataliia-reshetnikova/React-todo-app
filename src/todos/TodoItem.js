37a3efb7dbacd725194fe321d5f42eb34962e77a
nataliereshetnikova <ab1748@gmail.com>
2020-05-28 14:10:13 -0400

    toggle checkbox effect

--- src/todos/TodoItem.js
+++ src/todos/TodoItem.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 
 function TodoItem({todo, index, onChange}){
+    const classes = [];
+    if(todo.completed){
+        classes.push('done');
+    }
     return(
-        <li>
-            <span>
+        <li >
+            <span className={classes.join(" ")}>
                 <input type="checkbox" onChange={()=>onChange(todo.id)}/>
                 &nbsp;
                 <strong>{index+1}.</strong>
